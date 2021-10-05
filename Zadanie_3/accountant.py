6bf4c0b8015fabf49c45c2c920598a3889c3d796
Tobiasz Wójcik <tbaztw@gmail.com>
2021-10-06 01:23:13 +0200

    Obsługa w konsoli saldo, przegląd, magazyn

--- Zadanie_3/accountant.py
+++ Zadanie_3/accountant.py
@@ -10,14 +10,14 @@ stan_magazynowy = {} # identyfikator: liczba_sztuk
 
 def dzialanie_magazyn(identyfikator, liczba_sztuk):
     if stan_magazynowy.get(identyfikator) == None:
-        stan_magazynowy[identyfikator]=liczba_sztuk
+        stan_magazynowy[identyfikator] = liczba_sztuk
     else:
-        stan_magazynowy.update(identyfikator = stan_magazynowy.get(identyfikator)+liczba_sztuk)
-    if (stan_magazynowy.get(identyfikator)-liczba_sztuk<0):
+        stan_magazynowy[identyfikator] = stan_magazynowy.get(identyfikator)+liczba_sztuk
+    if (stan_magazynowy.get(identyfikator)<0):
             print("Brak wystarczającej ilości produktu {} w magazynie".format(identyfikator))
 
 def zapis_akcji(akcja, parametry):
-    zapis_zdarzen.append({'akcja': akcja,'parametry': (parametry) })
+    zapis_zdarzen.append({'akcja': akcja,'parametry': tuple(parametry) })
 
 def test(tekst):
     if is_test:
@@ -64,7 +64,7 @@ while act == "saldo" or act == "sprzedaż" or act == "zakup" or act == "stop":
         liczba_sztuk = int(action.pop(1))
         if liczba_sztuk < 0:
             print("Podana liczba sztuk jest mniejsza od zera")
-        if action == "sprzedaż":
+        if act == "sprzedaż":
             sprzedaz(identyfikator, wartosc_jednostkowa, liczba_sztuk)
         else:
             zakup(identyfikator, wartosc_jednostkowa, liczba_sztuk)
@@ -93,7 +93,44 @@ while act == "saldo" or act == "sprzedaż" or act == "zakup" or act == "stop":
     # elif action == "przedląd":
     #     pass
     elif act == "stop":
-        pass
+        while True:
+            act = input("Podaj operację do wykonania: ")
+            act = act.strip()
+            if act == "saldo":
+                wartosc = int(input("Podaj wartość salda: "))
+                test(wartosc)
+                komentarz = input("Podaj komentarz do wpisu: ")
+                test(komentarz)
+                saldo(wartosc, komentarz)
+            elif act == "sprzedaż" or act == "zakup":
+                identyfikator = input("Podaj identyfikator produktu: ")
+                wartosc_jednostkowa = int(input("Podaj cenę jednostkową w groszach: "))
+                if wartosc_jednostkowa < 0:
+                    print("Podana wartość jednostkowa jest mniejsza od zera")
+                liczba_sztuk = int(input("Podaj liczbę sztuk: "))
+                if liczba_sztuk < 0:
+                    print("Podana liczba sztuk jest mniejsza od zera")
+                if act == "sprzedaż":
+                    sprzedaz(identyfikator.strip(), wartosc_jednostkowa, liczba_sztuk)
+                else:
+                    zakup(identyfikator.strip(), wartosc_jednostkowa, liczba_sztuk)
+            elif act == "konto":
+                print("Obecny stan konta to {} groszy".format(saldo_kwota))
+                break
+            elif act == "magazyn":
+                produkty = input("Podaj produkty do wypisania, identyfikatory oddziel przecinkiem\",\"")
+                produkty = produkty.split(",")
+                for idx, produkt in enumerate(produkty):
+                    produkt = produkt.strip()
+                    print("{}. {} sztuk {}".format(idx+1, produkt, stan_magazynowy.get(produkt)))
+                print(produkty)
+                break
+            elif act == "przegląd":
+                test("Przegląd")
+                for idx, wydazenie in enumerate(zapis_zdarzen):
+                    print("{}. {}".format(idx+1, wydazenie))
+            else:
+                break
     del action[0]
     if len(action):
         act = action[0]
@@ -104,6 +141,6 @@ else:
         pass
     else:
         print("Niedozwolona akcja !!")
-print (action_cp)
-print (saldo_kwota)
-print (stan_magazynowy)
+test(action_cp)
+test(saldo_kwota)
+test(stan_magazynowy)
