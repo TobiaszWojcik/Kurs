4b55fbfc1213adedd69725613c21ef807fe0e48d
Tobiasz Wójcik <tbaztw@gmail.com>
2021-10-16 00:06:03 +0200

    Dodanie rozwiązania klasa pies

--- Zadanie_5/dodatkowe.py
+++ Zadanie_5/dodatkowe.py
@@ -7,6 +7,22 @@
 # Stwórz klasę pies, wiedząc, że pies może mieć charakterystyczne imię, rasę, kolor sierści,
 # a ponadto powinien móc szczekać i wyć.
 
+class Pies():
+    def __init__(self, imie, rasa, kolor_siersci):
+        self.imie = imie
+        self.rasa = rasa
+        self.kolor_siersci = kolor_siersci
+
+    def szczekaj(self):
+        print("Hał Hał")
+
+    def wyj(self):
+        print("Auuuuuuu")
+
+pies = Pies("Reksio", "kundel", "łaciaty")
+pies.szczekaj()
+pies.wyj()
+
 # Stwórz klasę betoniarka. Betoniarka ma swoją charakterystyczną pojemność (w litrach)
 # Betoniarka może być podłączona lub może nie być podłączona do prądu, powinna mieć funkcję wyłączenia.
 # Betoniarka może, ale nie musi zostać napełniona piachem, cementem i wodą.
