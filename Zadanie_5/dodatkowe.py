627e085d25774068782da81b2d80700ebd485e32
Tobiasz Wójcik <tbaztw@gmail.com>
2021-10-15 23:56:31 +0200

    Plik z zadaniami dodatkowymi dodatkowe.py

---
+++ Zadanie_5/dodatkowe.py
@@ -0,0 +1,16 @@
+# Funkcje
+# Stwórz funkcję, która przyjmuje dwie liczby całkowite, które są granicami przedziału
+# liczbowego i zwraca informację czy w tym przedziale są liczby podzielne przez 3
+# i ile jest tam takich liczb. Przykładowo func(1,7) zwróci (True, 2).
+
+# Klasy
+# Stwórz klasę pies, wiedząc, że pies może mieć charakterystyczne imię, rasę, kolor sierści,
+# a ponadto powinien móc szczekać i wyć.
+
+# Stwórz klasę betoniarka. Betoniarka ma swoją charakterystyczną pojemność (w litrach)
+# Betoniarka może być podłączona lub może nie być podłączona do prądu, powinna mieć funkcję wyłączenia.
+# Betoniarka może, ale nie musi zostać napełniona piachem, cementem i wodą.
+# Betoniarka przygotuje i wyrzuci cement tylko wtedy, gdy ma zarówno wodę,
+# jak i cement, i piach. Dodatkowo: betoniarka wyższej klasy informuje o jakości cementu, gdy
+# go wyrzuca. Cement dobrej jakości jest tylko wtedy, gdy spełniono warunki proporcji wody do cementu
+# do piachu w stosunku 5:2:3.
