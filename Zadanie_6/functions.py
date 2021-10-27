b90e25c3c19f2e4385009dea8901f42bdd7d2b80
Tobiasz Wójcik <tbaztw@gmail.com>
2021-10-27 21:20:25 +0200

    Dodanie obsługi błędu braku pliku źródłowego, komentarze do funkcji i klas

--- Zadanie_6/functions.py
+++ Zadanie_6/functions.py
@@ -2,22 +2,26 @@ import csv
 import os
 
 
+# Funkcja drukująca zawartość listy
 def csv_print(file, spacer=", "):
     for row in file:
         print(spacer.join(row))
 
 
+# Funkcja zmieniająca zawartość kolumny w liście
 def swipe_content(content, changes):
     for change in changes:
         content[int(change[0])][int(change[1])] = change[2]
     return content
 
 
+# Klasa obsługująca wczytywanie oraz zapis pliku csv
 class FileHandler:
     def __init__(self):
         self.path_in = None
         self.path_out = None
 
+    # Metoda odczytu pliku csv, zwraca listę lub False w przypadku braku pliku
     def loadfile(self, path_in):
         self.path_in = path_in
         content = []
@@ -28,9 +32,10 @@ class FileHandler:
                     content.append(row)
             return content
         else:
-            print("Brak pliku źródłowego")
+            print('Brak pliku o podanej nazwie: "{}"'.format(self.path_in))
             return False
 
+    # Metoda zapisu pliku
     def savefile(self, content, path_out=None):
         if path_out is None:
             path_out = self.path_in
--- Zadanie_6/reader.py
+++ Zadanie_6/reader.py
@@ -5,15 +5,13 @@ from functions import swipe_content
 
 path_in = sys.argv[1]
 path_out = sys.argv[2]
-changes=[]
+changes = []
 for change in sys.argv[3:]:
     changes.append(change.split(","))
 
 openfile = FileHandler()
 content = (openfile.loadfile(path_in))
-
-swipe_content(content, changes)
-
-csv_print(content)
-
-openfile.savefile(content, path_out)
+if content:
+    swipe_content(content, changes)
+    csv_print(content)
+    openfile.savefile(content, path_out)
