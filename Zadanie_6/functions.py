88b4a0be1368b0e8bc503ed9365c253b26512260
Tobiasz Wójcik <tbaztw@gmail.com>
2021-10-28 23:09:31 +0200

    Dodanie obsłubi sprawdzania i tworzenia podfolderów, zmiana adresu zapisywania plików, obsługa podania argumentów z poza listy w pliku csv

--- Zadanie_6/functions.py
+++ Zadanie_6/functions.py
@@ -2,6 +2,20 @@ import csv
 import os
 
 
+# Funkcja rekurencyjna sprawdza w dół ostatni istniejący folder i zwraca jego pozycję.
+# Parametr create decyduje czy funkcja tworzy podfoldery aż do pierwotnie podanej lokalizacji
+def check_dir_exist(path, create=False):
+    pathabs = os.path.abspath(path)
+    if not os.path.isdir(pathabs):
+        dip = check_dir_exist(os.path.split(path)[0], create)
+        if create:
+            if not os.path.split(path)[1] == "":
+                os.mkdir(path)
+        return dip
+
+    return pathabs
+
+
 # Funkcja drukująca zawartość listy
 def csv_print(file, spacer=", "):
     for row in file:
@@ -10,20 +24,28 @@ def csv_print(file, spacer=", "):
 
 # Funkcja zmieniająca zawartość kolumny w liście
 def swipe_content(content, changes):
+    max_row = len(content)
+    max_col = len(content[0])
+    print(max_row, max_col)
     for change in changes:
-        content[int(change[0])][int(change[1])] = change[2]
+        if max_row > int(change[0]) and max_col > int(change[1]):
+            content[int(change[0])][int(change[1])] = change[2]
+        else:
+            print("Brak możliwość zmiany komurki {}x{} na wartość:{}".format(change[0], change[1], change[2]))
     return content
 
 
 # Klasa obsługująca wczytywanie oraz zapis pliku csv
 class FileHandler:
     def __init__(self):
-        self.path_in = None
-        self.path_out = None
+        self.path_in = ""
+        self.path_out = ""
+        self.filename = ""
 
     # Metoda odczytu pliku csv, zwraca listę lub False w przypadku braku pliku
     def loadfile(self, path_in):
         self.path_in = path_in
+        self.filename = os.path.basename(path_in)
         content = []
         if os.path.exists(self.path_in):
             with open(self.path_in, 'r') as file:
@@ -33,6 +55,7 @@ class FileHandler:
             return content
         else:
             print('Brak pliku o podanej nazwie: "{}"'.format(self.path_in))
+
             return False
 
     # Metoda zapisu pliku
@@ -40,5 +63,8 @@ class FileHandler:
         if path_out is None:
             path_out = self.path_in
         self.path_out = path_out
-        with open(self.path_out, 'w') as file:
+        check_dir_exist(path_out, True)
+        final_dir = os.path.join(self.path_out, self.filename)
+        print(final_dir)
+        with open(final_dir, 'w') as file:
             csv.writer(file).writerows(content)
--- Zadanie_6/reader.py
+++ Zadanie_6/reader.py
@@ -1,17 +1,28 @@
 import sys
+import os
 from functions import FileHandler
 from functions import csv_print
 from functions import swipe_content
+from functions import check_dir_exist
+
 
 path_in = sys.argv[1]
-path_out = sys.argv[2]
-changes = []
-for change in sys.argv[3:]:
-    changes.append(change.split(","))
+if os.path.isfile(path_in):
+    path_out = sys.argv[2]
+    changes = []
+    dirout = check_dir_exist(path_out, True)
+    print(dirout, "to wyszlo")
+    for change in sys.argv[3:]:
+        changes.append(change.split(","))
+    openfile = FileHandler()
+    content = (openfile.loadfile(path_in))
 
-openfile = FileHandler()
-content = (openfile.loadfile(path_in))
-if content:
-    content = swipe_content(content, changes)
-    csv_print(content)
-    openfile.savefile(content, path_out)
+    if content:
+        content = swipe_content(content, changes)
+        csv_print(content)
+        openfile.savefile(content, path_out)
+else:
+    if os.path.isdir(path_in):
+        print(os.listdir(path_in))
+    else:
+        print(os.listdir(check_dir_exist(path_in)))
